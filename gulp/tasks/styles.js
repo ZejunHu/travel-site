2fccb68abd7b1c6bb4112d2b998005264bd8256d
Jonathan <jonathanzejunhu@gmail.com>
2017-07-24 11:58:13 -0400

    after mobile first

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,11 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
   .on('error', function(errorInfo) {
     console.log(errorInfo.toString());
     this.emit('end');
